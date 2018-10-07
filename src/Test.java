7267c5d6b1301e7482cf17a6d18c5371d04cd17b
wjy <690397098@qq.com>
2018-10-08 03:34:27 +0800

    update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 
 public class Test {
-    //对于一个整数N（512 <= N <= 1024），计算2的N次方并在屏幕显示十进制结果。
+    //对于一个整数N（512 <= N <= 1024），计算2的N次方并在屏幕显示十进制结果。西UI徐习
     public static void main(String[] args){
  System.out.println(Math.random()*200);       
 }
